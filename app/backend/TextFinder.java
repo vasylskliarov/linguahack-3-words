be3d8bf49c2fed6fcdef48900bca0b40f1842e45
gleb23 <hlibbabii@gmail.com>
2015-10-03 20:40:20 +0300

    update stub with real call to getPlainTextById

--- app/backend/TextFinder.java
+++ app/backend/TextFinder.java
@@ -8,12 +8,16 @@ import models.Word;
 
 public class TextFinder {
 
+    private static final String DUMMY = "Suicide is when a person chooses to kill himself or herself. When someone kills himself, people say that he has \"committed suicide\". When a person thinks about killing themselves, the person is said to be suicidal.\n" +
+            "When people start having thoughts about killing themselves it is, or should be, a medical emergency. They should get a suicide risk assessment as soon as possible.\n" +
+            "There are many reasons why a person might think about committing suicide. Most people who are suicidal have some type of mental condition or illness. They may have a chronic condition, which means it has been going on for a long time. But it may be an acute condition – which means the first symptoms of mental illness happened rather quickly.\n";
+
     public static Long getTextId(Map<String, Double> features, Map<Long, Integer> shownFiles) {
         return 42L;
     }
 
     public static String getPlainTextById(Long id){
-        return "test";
+        return DUMMY;
     }
 
     public static List<Word> getNormalizedWordsByTextId(Long id){
