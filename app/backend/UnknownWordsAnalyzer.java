a650a8751dc3b6a15ebd5b861b41ca2d1b659eb1
gleb23 <hlibbabii@gmail.com>
2015-10-03 20:17:10 +0300

    add method for updated words statistics

--- app/backend/UnknownWordsAnalyzer.java
+++ app/backend/UnknownWordsAnalyzer.java
@@ -2,7 +2,7 @@ package backend;
 
 import models.Word;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by hlib on 03.10.15.
@@ -10,5 +10,20 @@ import java.util.Map;
 public class UnknownWordsAnalyzer {
     public static Map<String, Double> getFeaturesForUser() {
         return Word.getWordStatistics();
+    }
+
+    public static void updateWordsStatistics(List<String> encounteredWords, List<String> unknownWords) {
+        Set<String> encounteredWordsSet = new HashSet<>(encounteredWords);
+        Set<String> unknownWordsSet = new HashSet<>(unknownWords);
+        encounteredWordsSet.stream().forEach(w -> {
+            Word word = Word.findByValue(w);
+            word.setShowedCount(word.getShowedCount() + 1);
+            word.update();
+        });
+        unknownWordsSet.stream().forEach(w -> {
+            Word word = Word.findByValue(w);
+            word.setUnknownCount(word.getUnknownCount() + 1);
+            word.update();
+        });
     }
 }
