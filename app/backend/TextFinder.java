b2c775ed1e3b5a6beb1562b679abe1421936d815
Bohdan <BohdanLevitsky@gmail.com>
2015-10-03 10:04:01 -0700

    getPlainTextById, getNormalizedTextById added

--- app/backend/TextFinder.java
+++ app/backend/TextFinder.java
@@ -1,5 +1,7 @@
 package backend;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import play.Play;
@@ -8,5 +10,13 @@ public class TextFinder {
 
     public static Long getTextId(Map<String, Double> features, Map<Long, Integer> shownFiles) {
         return 42L;
+    }
+
+    public String getPlainTextById(Long id){
+        return "test";
+    }
+
+    public List<String> getNormalizedTextById(Long id){
+        return Collections.emptyList();
     }
 }
