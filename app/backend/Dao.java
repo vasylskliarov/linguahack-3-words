c6bd4a9f05bf7d0cb595ad5ec501709c4690a1b3
gleb23 <hlibbabii@gmail.com>
2015-10-04 02:19:12 +0300

    add missing dao

---
+++ app/backend/Dao.java
@@ -0,0 +1,16 @@
+package backend;
+
+import models.Text;
+import models.Word;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by hlib on 03.10.15.
+ */
+public class Dao {
+    public List<Text> getAllTexts() {
+        return Text.findAll();
+    }
+}
