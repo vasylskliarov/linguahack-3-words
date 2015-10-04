35b01610428dbff0c6a2b4a85b3c3106488a0275
gleb23 <hlibbabii@gmail.com>
2015-10-04 12:13:05 +0300

    make getting random text id determined

--- app/backend/Dao.java
+++ app/backend/Dao.java
@@ -5,17 +5,27 @@ import models.Word;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by hlib on 03.10.15.
  */
 public class Dao {
+
+    private final static Long[] STUB_TEXT_IDS = {-592328L, -1049090L, -1618176L};
+    final Random random = new Random();
+
     public List<Text> getAllTexts() {
         return Text.findAll();
     }
 
     public Long getRandomTextId() {
         // call Text method to query random text id
-        return 42L;
+        return getStubTextId();
+    }
+
+    private Long getStubTextId() {
+        int index = random.nextInt(STUB_TEXT_IDS.length);
+        return STUB_TEXT_IDS[index];
     }
 }
